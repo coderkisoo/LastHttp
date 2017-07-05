f49c1daa2e90a2309e09d97e6b422950515a794a
CoderKiSoo <coderkisoo@gmail.com>
2017-07-06 01:01:39 +0800

    address httpurl

--- lasthttp/src/main/java/cn/kisoo/lasthttp/Address.java
+++ lasthttp/src/main/java/cn/kisoo/lasthttp/Address.java
@@ -26,25 +26,52 @@ public final class Address {
     final ProxySelector proxySelector;
     final @Nullable Proxy proxy;
     final @Nullable SSLSocketFactory sslSocketFactory;
-
+    final @Nullable HostnameVerifier hostnameVerifier;
+    final @Nullable CertificatePinner certificatePinner;
 
     public Address(String uriHost, int uriPort, Dns dns, SocketFactory socketFactory,
                    @Nullable SSLSocketFactory sslSocketFactory, @Nullable HostnameVerifier hostnameVerifier,
                    @Nullable CertificatePinner certificatePinner, Authenticator proxyAuthenticator,
                    @Nullable Proxy proxy, List<Protocol> protocols, List<ConnectionSpec> connectionSpecs,
                    ProxySelector proxySelector) {
-        this.url = url;
+        this.url = new HttpUrl.Builder()
+                .scheme(sslSocketFactory != null ? "https" : "http")
+                .host(uriHost)
+                .port(uriPort)
+                .build();
         if (dns == null) throw new NullPointerException("dns == null");
         this.dns = dns;
-        if (socketFactory== null) throw new NullPointerException("socketFactory == null");
+        if (socketFactory == null) throw new NullPointerException("socketFactory == null");
         this.socketFactory = socketFactory;
         if (proxyAuthenticator == null) throw new NullPointerException("proxyAuthenticator == null");
         this.proxyAuthenticator = proxyAuthenticator;
-        if (protocols==null) throw new NullPointerException("protocols == null");
+        if (protocols == null) throw new NullPointerException("protocols == null");
         this.protocols = protocols;
         this.connectionSpecs = connectionSpecs;
         this.proxySelector = proxySelector;
         this.proxy = proxy;
         this.sslSocketFactory = sslSocketFactory;
+        this.hostnameVerifier = hostnameVerifier;
+        this.certificatePinner = certificatePinner;
+    }
+
+    public HttpUrl url(){
+        return this.url;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder()
+                .append("Address{")
+                .append(url.host()).append(":").append(url.port());
+
+        if (proxy != null) {
+            result.append(", proxy=").append(proxy);
+        } else {
+            result.append(", proxySelector=").append(proxySelector);
+        }
+
+        result.append("}");
+        return result.toString();
     }
 }
--- lasthttp/src/main/java/cn/kisoo/lasthttp/HttpUrl.java
+++ lasthttp/src/main/java/cn/kisoo/lasthttp/HttpUrl.java
@@ -1,7 +1,86 @@
 package cn.kisoo.lasthttp;
 
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+
 /**
  * Created by kangqizhou on 2017/5/29.
  */
 public class HttpUrl {
+
+    public static final String SCHEME_HTTP = "http";
+    public static final String SCHEME_HTTPS = "https";
+
+    final String scheme;
+    final String host;
+    final int port;
+    final String url;
+
+    private HttpUrl(Builder builder) {
+        this.scheme = builder.scheme;
+        this.host = builder.host;
+        this.port = builder.effectivePort();
+        this.url = builder.toString();
+    }
+
+    public String host() {
+        return host;
+    }
+
+    public int port() {
+        return port;
+    }
+
+    public static final class Builder {
+        @Nullable
+        String scheme;
+        @Nullable
+        String host;
+        int port;
+
+        public int effectivePort() {
+            return port == -1 ? defaultPort(scheme) : port;
+        }
+
+        private int defaultPort(String scheme) {
+            switch (scheme) {
+                case SCHEME_HTTP:
+                    return 80;
+                case SCHEME_HTTPS:
+                    return 443;
+                default:
+                    return -1;
+            }
+        }
+
+        public Builder scheme(String scheme) {
+            if (TextUtils.isEmpty(scheme))
+                throw new NullPointerException("scheme == null");
+            if (scheme.equalsIgnoreCase(SCHEME_HTTP))
+                this.scheme = SCHEME_HTTP;
+            else if (scheme.equalsIgnoreCase(SCHEME_HTTPS))
+                this.scheme = SCHEME_HTTPS;
+            else
+                throw new IllegalArgumentException("unexpected scheme:" + scheme);
+            this.scheme = scheme;
+            return this;
+        }
+
+        public Builder host(String host) {
+            this.host = host;
+            return this;
+        }
+
+        public Builder port(int port) {
+            if (port <= 0 || port > 65535) throw new IllegalArgumentException("unexpected port: " + port);
+            this.port = port;
+            return this;
+        }
+
+        public HttpUrl build() {
+            if (scheme == null) throw new IllegalStateException("scheme == null");
+            if (host == null) throw new IllegalStateException("host == null");
+            return new HttpUrl(this);
+        }
+    }
 }
