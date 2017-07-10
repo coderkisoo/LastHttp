c785279ea1ef2e578d7c15637bd03dc8ad9d0342
CoderKiSoo <coderkisoo@gmail.com>
2017-07-10 23:12:38 +0800

    HTTPHeaders

--- lasthttp/src/main/java/cn/kisoo/lasthttp/HttpHeaders.java
+++ lasthttp/src/main/java/cn/kisoo/lasthttp/HttpHeaders.java
@@ -1,11 +1,18 @@
 package cn.kisoo.lasthttp;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * Created by kangqizhou on 2017/7/10.
  */
 public class HttpHeaders {
+
+    private static final String TOKEN = "([^ \"=]*)";
+    private static final String QUOTED_STRING = "\"([^\"]*)\"";
+    private static final Pattern PARAMETER
+            = Pattern.compile(" +" + TOKEN + "=(:?" + QUOTED_STRING + "|" + TOKEN + ") *(:?,|$)");
+
     public static List<Challenge> parseChallenges(Headers headers, String responseField) {
         return null;
     }
