d3726fcd671bd4b6c19146d91be6534a350a2260
CoderKiSoo <coderkisoo@gmail.com>
2017-09-12 21:08:15 +0800

    Connection

--- lasthttp/src/main/java/cn/kisoo/lasthttp/Connection.java
+++ lasthttp/src/main/java/cn/kisoo/lasthttp/Connection.java
@@ -1,8 +1,31 @@
 package cn.kisoo.lasthttp;
 
+import java.net.Socket;
+
 /**
  * Created by kangqizhou on 2017/5/29.
  */
 
-public class Connection {
+public interface Connection {
+
+    /***
+     * 返回当前连接的路由
+     * @return
+     */
+    Route route();
+
+    /***
+     * 返回当前连接所用的connection
+     * @return
+     */
+    Socket socket();
+
+
+    Protocol protocol();
+
+    /***
+     * 如果是https，返回tls握手的handshake。如果不是https，返回null
+     * @return
+     */
+    Handshake handshake();
 }
