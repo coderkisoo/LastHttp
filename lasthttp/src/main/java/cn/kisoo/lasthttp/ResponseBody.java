70711dac1da0a327bf09acb066be2d29f3f4e8de
CoderKiSoo <coderkisoo@gmail.com>
2017-10-26 19:18:10 +0800

    MediaType

--- lasthttp/src/main/java/cn/kisoo/lasthttp/ResponseBody.java
+++ lasthttp/src/main/java/cn/kisoo/lasthttp/ResponseBody.java
@@ -2,6 +2,8 @@ package cn.kisoo.lasthttp;
 
 import android.support.annotation.Nullable;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Reader;
 
 /**
@@ -13,5 +15,19 @@ public abstract class ResponseBody implements Cloneable{
     private Reader reader;
 
     public abstract  @Nullable MediaType contentType();
+
+    public abstract long contentLength();
+
+    public final InputStream byteStream() {
+        ;
+    }
+
+    public final byte[] bytes() throws IOException {
+        long contentLength = contentLength();
+        if (contentLength > Integer.MAX_VALUE){
+            throw new IOException("Cannot buffer entire body for content length:"+ contentLength);
+        }
+
+    }
 
 }
