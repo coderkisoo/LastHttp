6d465d078842824063a4b54cfd07bf2cb2184a3e
CoderKiSoo <coderkisoo@gmail.com>
2017-10-26 19:18:35 +0800

    MediaType

--- lasthttp/src/main/java/cn/kisoo/lasthttp/ResponseBody.java
+++ lasthttp/src/main/java/cn/kisoo/lasthttp/ResponseBody.java
@@ -10,24 +10,26 @@ import java.io.Reader;
  * Created by kangqizhou on 2017/7/7.
  */
 
-public abstract class ResponseBody implements Cloneable{
+public abstract class ResponseBody implements Cloneable {
 
     private Reader reader;
 
-    public abstract  @Nullable MediaType contentType();
+    public abstract
+    @Nullable
+    MediaType contentType();
 
     public abstract long contentLength();
 
     public final InputStream byteStream() {
-        ;
+        return null;
     }
 
     public final byte[] bytes() throws IOException {
         long contentLength = contentLength();
-        if (contentLength > Integer.MAX_VALUE){
-            throw new IOException("Cannot buffer entire body for content length:"+ contentLength);
+        if (contentLength > Integer.MAX_VALUE) {
+            throw new IOException("Cannot buffer entire body for content length:" + contentLength);
         }
-
+        return null;
     }
 
 }
